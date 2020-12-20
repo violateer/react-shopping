2929f5f27682ee2765bcea886c23b0a5731ac002
violateer <1828257089@qq.com>
2020-12-20 20:35:30 +0800

    完成路由设计

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,26 @@
 // 引入样式
 import { Container } from 'react-bootstrap';
+// 路由
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </>
-  );
+function App () {
+    return (
+        <Router>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <Route path="/" component={HomeScreen} exact/>
+                    <Route path="/products/:id" component={ProductScreen}/>
+                </Container>
+            </main>
+            <Footer/>
+        </Router>
+    );
 }
 
 export default App;
---
+++ front/src/screens/ProductScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductScreen = () => {
+    return (
+        <div>产品详情</div>
+    );
+};
+
+export default ProductScreen;
