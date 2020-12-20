f4f1322d6a819a24004a00172b86d7edb879efdd
violateer <1828257089@qq.com>
2020-12-20 20:10:03 +0800

    创建评分组件

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -4,18 +4,18 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
-  return (
-    <>
-      <h1>最新产品</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </>
-  );
+    return (
+        <>
+            <h1>最新产品</h1>
+            <Row>
+                {products.map((product) => (
+                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    );
 };
 
 export default HomeScreen;
