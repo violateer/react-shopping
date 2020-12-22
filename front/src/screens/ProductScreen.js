7c0c11e98db03fa48fb26efcc407cea5250e84c0
violateer <1828257089@qq.com>
2020-12-22 13:19:05 +0800

    添加数量下拉框及完善添加购物车按钮

--- front/src/screens/ProductScreen.js
+++ front/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,13 +7,20 @@ import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+    const [qty, setQty] = useState(1);
     const dispatch = useDispatch();
     const productDetails = useSelector(state => state.productDetails);
     const { loading, error, product } = productDetails;
     useEffect(() => {
         dispatch(listProductDetails(match.params.id));
     }, [dispatch, match]);
+    
+    // 添加到购物车事件
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`);
+    };
+    
     return (
         <>
             {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
@@ -48,7 +55,23 @@ const ProductScreen = ({ match }) => {
                                         </Row>
                                     </ListGroup.Item>
                                     <ListGroup.Item>
-                                        <Button className='btn-block'
+                                        <Row>
+                                            <Col>数量：</Col>
+                                            <Col>
+                                                {/* 待添加库存为零的处理 */}
+                                                <Form.Control as='select' value={qty}
+                                                              onChange={e => setQty(e.target.value)}>
+                                                    {[...Array(product.countInStock).keys()].map(i => (
+                                                            <option value={i + 1} key={i + 1}>{i + 1}</option>
+                                                        )
+                                                    )}
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Button onClick={addToCartHandler}
+                                                className='btn-block'
                                                 type="button"
                                                 disabled={!product.countInStock}>添加到购物车</Button>
                                     </ListGroup.Item>
