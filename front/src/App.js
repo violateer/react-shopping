0bf9a3170391b838c51bf26785bc4fe98bac0a45
violateer <1828257089@qq.com>
2020-12-22 22:43:39 +0800

    完成用户登录页面及登录功能

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App () {
     return (
@@ -18,6 +19,7 @@ function App () {
                     <Route path="/" component={HomeScreen} exact/>
                     <Route path="/products/:id" component={ProductScreen}/>
                     <Route path="/cart/:id?" component={CartScreen}/>
+                    <Route path="/login" component={LoginScreen}/>
                 </Container>
             </main>
             <Footer/>
