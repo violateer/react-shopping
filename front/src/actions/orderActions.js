a73110a19acd79ff69f2c27cc3578f20cdc9445d
violateer <1828257089@qq.com>
2020-12-23 22:43:58 +0800

    创建获取订单reducer&&action

--- front/src/actions/orderActions.js
+++ front/src/actions/orderActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from '../constants/orderConstants';
+import {
+    ORDER_CREATE_FAIL,
+    ORDER_CREATE_REQUEST,
+    ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL,
+    ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS
+} from '../constants/orderConstants';
 
 // 创建订单action
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -20,6 +25,30 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch (err) {
         dispatch({
             type: ORDER_CREATE_FAIL,
+            payload: err.response
+                     && err.response.data.message ? err.response.data.message : err.message
+        });
+    }
+};
+
+// 获取订单action
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_DETAILS_REQUEST });
+        // 获取登陆用户的信息
+        const { userLogin: { userInfo } } = getState();
+        
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+        
+        const { data } = await axios.get(`/api/orders/${id}`, config);
+        dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data });
+    } catch (err) {
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
             payload: err.response
                      && err.response.data.message ? err.response.data.message : err.message
         });
--- front/src/reducers/orderReducers.js
+++ front/src/reducers/orderReducers.js
@@ -1,4 +1,9 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from '../constants/orderConstants';
+import {
+    ORDER_CREATE_FAIL,
+    ORDER_CREATE_REQUEST,
+    ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL,
+    ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS
+} from '../constants/orderConstants';
 
 // 创建订单reducer
 export const orderCreateReducer = (state = {}, action) => {
@@ -14,6 +19,29 @@ export const orderCreateReducer = (state = {}, action) => {
                 success: true
             };
         case ORDER_CREATE_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+// 获取订单reducer
+export const orderDetailsReducer = (state = { orderItems: [], shippingAddress: {} }, action) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true
+            };
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload
+            };
+        case ORDER_DETAILS_FAIL:
             return {
                 loading: false,
                 error: action.payload
