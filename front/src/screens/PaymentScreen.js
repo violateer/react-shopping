f0b12154cba412bac9e51bac6b7626788c0c423c
violateer <1828257089@qq.com>
2020-12-25 18:32:25 +0800

    添加Paypal支付方式选项

--- front/src/screens/PaymentScreen.js
+++ front/src/screens/PaymentScreen.js
@@ -31,10 +31,10 @@ const PaymentScreen = ({ history }) => {
                 <Form.Group>
                     <Form.Label as='legend'>选择支付方式</Form.Label>
                     <Col>
-                        <Form.Check type='radio' label='微信' id='微信' name='paymentMethod' value='微信' checked
+                        <Form.Check type='radio' label='微信' id='wechat' name='paymentMethod' value='微信' checked
                                     onChange={e => setPaymentMethod(e.target.value)}></Form.Check>
-                        {/*<Form.Check type='radio' label='支付宝' id='支付宝' name='paymentMethod' value='支付宝'*/}
-                        {/*            onChange={e => setPaymentMethod(e.target.value)}></Form.Check>*/}
+                        <Form.Check type='radio' label='PayPal' id='PayPal' name='paymentMethod' value='PayPal'
+                                    onChange={e => setPaymentMethod(e.target.value)}></Form.Check>
                     </Col>
                 </Form.Group>
                 <Button type='submit' variant='primary'>
