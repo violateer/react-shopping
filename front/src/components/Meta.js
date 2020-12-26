1ffd75d16badc4c5101979b683297a24daf17a2d
violateer <1828257089@qq.com>
2020-12-26 16:56:32 +0800

    修复meta的keywords

--- front/src/components/Meta.js
+++ front/src/components/Meta.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const Meta = ({ title, description, keyword }) => {
+const Meta = ({ title, description, keywords }) => {
     return (
         <Helmet>
             <title>{title}</title>
             <meta name="description" content={description}/>
-            <meta name="keywords" content={keyword}/>
+            <meta name="keywords" content={keywords}/>
         </Helmet>
     );
 };
@@ -14,7 +14,7 @@ const Meta = ({ title, description, keyword }) => {
 Meta.defaultProps = {
     title: '欢迎来到V商城',
     description: '购好物，上V商城',
-    keyword: '电商，手机，便宜手机'
+    keywords: '电商，手机，便宜手机'
 };
 
 export default Meta;
